8335711c186739fac296e2dc1362dc04776e1210
AK-macbook <aalkhareji27@gmail.com>
2025-03-06 11:45:00 +0300

    Implement variables and string interpolation with personal details (message generated by ai)

--- src/variables.ts
+++ src/variables.ts
@@ -9,11 +9,11 @@
  * 5. image         -> assign it a value of a url of your image or ant image that represents you online
  */
 
-let fullName: string;
-let yearOfBirth: number;
-let hobby: string;
-let funFact: string;
-let image: string;
+let fullName: string = "Abdullah AlKhareji";
+let yearOfBirth: number = 1993;
+let hobby: string = "running";
+let funFact: string = "I'm a runner";
+let image: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK_mAcrV3vVhLq6HK4c1liqGV59qhOwXdEGw&s";
 
 /**
  * Part 2: String Interpolation
@@ -25,9 +25,9 @@ let image: string;
  * 3. hobbyString         -> assign it: "My hobby is {YOUR_HOBBY}""
  */
 
-let fullNameString: string;
-let ageString: string;
-let hobbyString: string;
+let fullNameString: string = `My name is ${fullName}`;
+let ageString: string = `I am ${new Date().getFullYear() - yearOfBirth}`;
+let hobbyString: string = `My hobby is ${hobby}`;
 
 /**
  * Part 3: Re-assignment
@@ -38,16 +38,20 @@ let hackerScore = 0;
 
 function incrementBy1() {
   // Increment hackerScore by 1 👇🏻
+  hackerScore++;
 }
 function decrementBy1() {
   // decrement hackerScore by 1 👇🏻
+  hackerScore--;
 }
 
 function incrementBy2() {
   // Increment hackerScore by 2 👇🏻
+  hackerScore += 2;
 }
 function decrementBy2() {
   // decrement hackerScore by 2 👇🏻
+  hackerScore -= 2;
 }
 
 // Ignore this part (:
